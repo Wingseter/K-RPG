e35f12cae67d5a2196f5cffe26164ecabedfe5fa
Wingseter <wingseter@gmail.com>
2021-04-04 18:48:45 +0900

    유저 스텟 생성 및 스텟창과 연결

---
+++ Assets/CharInfoFrame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharInfoFrame : MonoBehaviour
+{
+    public PlayerState player;
+
+    public TextMeshProUGUI lev;
+    public TextMeshProUGUI hp;
+    public TextMeshProUGUI atk;
+    public TextMeshProUGUI def;
+    public TextMeshProUGUI cri;
+   
+    public Image expBar;
+
+    private void OnEnable()
+    {
+        lev.text = player.lev.ToString();
+        hp.text = player.hp.ToString();
+        atk.text = player.atk.ToString();
+        def.text = player.def.ToString();
+        cri.text = string.Format("{0:0.00}",player.cri);
+
+        expBar.fillAmount = player.exp_Cur / player.exp_Max;
+    }
+}
---
+++ Assets/PlayerState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public int lev;     // 래밸
+
+    public float hp;    // 체력
+    public float hp_Cur;    // 현재 체력
+
+    public int atk;     // 공격력
+    public int def;     // 방어력
+    public float cri;   // 크리티컬
+
+    public float exp_Max;   // 맥스 경험치
+    public float exp_Cur;   // 현재 경험치
+
+    private void OnEnable()
+    {
+        hp_Cur = hp; 
+    }
+}
